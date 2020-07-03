8e2051833b85330d2afb0bf209bbb07c5b596b49
linkmanny <limanneng520@gmail.com>
2020-07-03 14:06:43 +0800

    project build

---
+++ Tetrix/Assets/Scripts/MainLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLogic : MonoBehaviour {
+
+    void Awake()
+    {
+        Debug.Log("Game Start"); 
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
